9223aa5654239cdcc01b8ba3849f6589b5f2e8d2
MrGreenzz <68908092+MrGreenzz@users.noreply.github.com>
2023-05-02 21:03:33 +0000

    started working on interface in App.java and deleted random cust file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class App{
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Welcome to XYZ bank.");
+        System.out.println("Please choose an option by entering the number next to it.");
+        System.out.println("1- Login /n2- Register");
+        Scanner scnr = new Scanner(System.in);
+        int option = scnr.nextInt();
+        
+        
     }
+
 }
