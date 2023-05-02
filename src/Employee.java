674bbd53c2d6c3ca18b9ca37dcb8f4e1f0f5c9a1
Abdulaziz <85900060+C3ZIZ@users.noreply.github.com>
2023-05-02 20:53:10 +0000

    Edited employee class

--- src/Employee.java
+++ src/Employee.java
@@ -6,5 +6,16 @@ public class Employee {
     private String userAddress;
     private String userName;
     private String userPassword;
+
+    public Employee ( String employeeName, String nationalID, String phoneNumber, String userAddress, String userName, String userPassword ) {
+        this.employeeName = employeeName;
+        this.nationalID = nationalID;
+        this.phoneNumber = phoneNumber;
+        this.userAddress = userAddress;
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
     
+
 }
