616a6fba352a80e20082e547eb2bfe2610ef8023
Abdulaziz <85900060+C3ZIZ@users.noreply.github.com>
2023-05-02 19:42:27 +0000

    Made the Employee file

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    
+    private String employeeName;
+    private String nationalID;
+    private String phoneNumber;
+    private String userAddress;
+    private String userName;
+    private String userPassword;
+    
+}
